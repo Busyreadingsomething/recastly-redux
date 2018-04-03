95eccd4a91ccaf99295d345b11ec8155c2fe5501
Makan Azarshahy <oro1919@gmail.com>
2018-04-03 14:55:16 -0700

    Added main reducer

--- src/reducers/main.js
+++ src/reducers/main.js
@@ -4,11 +4,4 @@ import videoList from './videoList.js';
 
 const rootReducer = combineReducers({ currentVideo, videoList });
 
-// rootReducer is suppose to "return" the value that comes from combineReducer
-// which will take the state and action and pass it between all the 
-// reducers
-
-// const rootReducer = () => combineReducers({ currentVideo, videoList });
-// need to invoke this one in order to get what we need
-
 export default rootReducer;
